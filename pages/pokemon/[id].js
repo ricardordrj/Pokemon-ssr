dcfb50868132c655ee486c972524d2aae0883b7f
Ricardo <ricardordrj@gmail.com>
2022-07-23 15:54:58 -0300

    initial client side base app

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,72 @@
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/Details.module.css";
+
+const Details = () => {
+    const {
+        query: { id },
+    } = useRouter();
+
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() => {
+        const getPokemon = async () => {
+            const res = await fetch(
+                `https://pokemon-ssr.s3.sa-east-1.amazonaws.com/pokemon-main/pokemon/${id}.json`
+            );
+
+            setPokemon(await res.json());
+        };
+        if (id) {
+            getPokemon();
+        }
+    }, [id]);
+
+    return (
+        <div>
+            <Head>
+                <title>{pokemon?.name}</title>
+            </Head>
+            <main>
+                <Link href="/">
+                    <a>Back</a>
+                </Link>
+                <section className={styles.layout}>
+                    <header>
+                        <img
+                            classname={styles.picture}
+                            src={`https://pokemon-ssr.s3.sa-east-1.amazonaws.com/pokemon-main/${pokemon?.image}`}
+                            alt={pokemon?.name}
+                        />
+                        <div>
+                            <p className={styles.name}>{pokemon?.name}</p>
+                            <p className={styles.type}>
+                                {pokemon?.type.join(", ")}
+                            </p>
+                        </div>
+                    </header>
+                    <table>
+                        <thead className={styles.header}>
+                            <tr>
+                                <th>Name</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {pokemon?.stats.map(({ name, value }) => (
+                                <tr key={name}>
+                                    <td className={styles.attribute}>{name}</td>
+                                    <td>{value}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default Details;
